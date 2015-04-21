213bdfe9e66479715796effbe6ec0cb8690b6658
ksascomm <ksasweb@jhu.edu>
2015-04-21 16:22:58 -0400

    revamped front page

--- front-page.php
+++ front-page.php
@@ -2,13 +2,21 @@
     $theme_option = flagship_sub_get_global_options();
     $news_quantity = $theme_option['flagship_sub_news_quantity'];
     $color_scheme = $theme_option['flagship_sub_color_scheme'];
-		if ( false === ( $news_query = get_transient( 'news_query' ) ) ) {
-			// It wasn't there, so regenerate the data and save the transient
-			$news_query = new WP_Query(array(
-				'post_type' => 'post',
-				'posts_per_page' => 6)); 
-				set_transient( 'news_query', $news_query, 2592000 );
-		} 	
+        //if ( false === ( $latest_post_query = get_transient( 'latest_post_query' ) ) ) {
+        // It wasn't there, so regenerate the data and save the transient
+        $latest_post_query = new WP_Query(array(
+            'post_type' => 'post',
+            'posts_per_page' => 1)); 
+            //set_transient( 'latest_post_query', $latest_post_query, 2592 );
+        //} 
+        if ( false === ( $news_query = get_transient( 'news_query' ) ) ) {
+            // It wasn't there, so regenerate the data and save the transient
+            $news_query = new WP_Query(array(
+                'post_type' => 'post',
+                'offset' => 1,
+                'posts_per_page' => 6)); 
+               // set_transient( 'news_query', $news_query, 2592000 );
+        }   
 
 ?>
 <?php if( get_header_image() != '' ) { ?>
@@ -21,13 +29,32 @@
 </div>
 <?php } ?>
 <div class="row wrapper radius10" id="page" role="main">
-	<div class="smal-12 columns">
-             <div class="panel callout radius10">
-                    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-                        <h4><?php the_title(); ?></h4>
-                        <?php the_content(); ?>
-                    <?php endwhile; endif; ?>
+	<div class="small-12 columns">
+ <?php if ( $latest_post_query->have_posts() ) : ?>
+                <div class="small-12 medium-6 columns">
+                    <h4>Latest <?php echo $theme_option['flagship_sub_feed_name']; ?></h4>
+            <?php while ($latest_post_query->have_posts()) : $latest_post_query->the_post(); ?>
+                    <div class="row post-container">
+                        <div class="small-11 columns centered post">
+                    <a href="<?php the_permalink(); ?>">
+                        <div class="row featured">
+                                <?php if ( has_post_thumbnail()) { the_post_thumbnail('large', array('align'=>'center')); }?>
+                        </div>
+                        <h5><?php the_title(); ?></h5>
+                        <?php the_excerpt(); ?>
+                    </a>
+                        </div>
+                    </div>
+            <?php endwhile; ?>
+                </div>
+            <?php endif; ?>
+            <div class="small-12 medium-5 columns panel callout radius10 last">
+                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                    <h4>About the <?php echo get_bloginfo ( 'description' ); ?> <?php echo get_bloginfo( 'title' ); ?></h4>
+                    <?php the_content(); ?>
+                <?php endwhile; endif; ?>
             </div>
+        </div>
 	    <?php if ( $news_query->have_posts() ) : ?>
 	        <div class="row">
              <h4><?php echo $theme_option['flagship_sub_feed_name']; ?></h4>
@@ -60,5 +87,4 @@
 		</div>
 		<?php endif; ?>
 	</div>
-</div>
 <?php get_footer(); ?>
