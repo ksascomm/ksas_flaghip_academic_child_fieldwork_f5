673f536822894eed839e623b6abb6ca1eae7391d
ksascomm <ksasweb@jhu.edu>
2017-02-15 13:20:36 -0500

    front-page content placed in <main> element

--- front-page.php
+++ front-page.php
@@ -20,47 +20,48 @@
 
         ?>
 
-<?php if( get_header_image() != '' ) { ?>
-<div class="row hide-for-mobile" role="img" aria-label="hero image area">
-    <div id="slider" class="small-12 columns radius10 no-gutter">
-        <div class="slide">
-            <img src="<?php header_image(); ?>" class="radius10" alt="header image"/>
-        </div>
-    </div>
-</div>
-<?php } ?>
-
-<div class="row wrapper radius10" id="page" role="main">
-	<div class="small-12 columns">
-        <?php if ( $latest_post_query->have_posts() ) : ?>
-                <div class="small-12 medium-6 columns">
-                    <h3>Latest <?php echo $theme_option['flagship_sub_feed_name']; ?></h3>
-            <?php while ($latest_post_query->have_posts()) : $latest_post_query->the_post(); ?>
-                    <div class="row post-container">
-                      <article class="small-11 columns centered post news-item" aria-labelledby="post-<?php the_ID(); ?>" itemscope="itemscope" itemtype="http://schema.org/BlogPosting" itemprop="blogPost">
-                            <div class="row featured">
-                                <?php the_post_thumbnail('large'); array('class'  => "floatleft", 'itemprop' => 'image'); ?>
-                            </div>
-                                <h1 itemprop="headline">
-                                    <a href="<?php the_permalink();?>" title="<?php the_title(); ?>" id="post-<?php the_ID(); ?>"><?php the_title(); ?></a>
-                                </h1>
-                                <div class="entry-content" itemprop="text"> 
-                                    <?php the_excerpt(); ?>
-                                </div>
-                        </article>
-                    </div>
-            <?php endwhile; ?>
-                </div>
-        <?php endif; ?>
-            <div class="small-12 medium-6 columns panel callout radius10 last">
-                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-                    <h1 class="page-title home">About the <?php echo get_bloginfo ( 'description' ); ?> <?php echo get_bloginfo( 'title' ); ?></h1>
-                    <?php the_content(); ?>
-                <?php endwhile; endif; ?>
+<main>
+    <?php if( get_header_image() != '' ) { ?>
+    <div class="row hide-for-mobile" aria-label="hero image area">
+        <div id="slider" class="small-12 columns radius10 no-gutter">
+            <div class="slide">
+                <img src="<?php header_image(); ?>" class="radius10" alt="header image"/>
             </div>
         </div>
-	    <?php if ( $news_query->have_posts() ) : ?>
-	        <div class="row">
+    </div>
+    <?php } ?>
+
+    <div class="row wrapper radius10" id="page">
+    	<div class="small-12 columns">
+            <?php if ( $latest_post_query->have_posts() ) : ?>
+                    <div class="small-12 medium-6 columns">
+                        <h3>Latest <?php echo $theme_option['flagship_sub_feed_name']; ?></h3>
+                <?php while ($latest_post_query->have_posts()) : $latest_post_query->the_post(); ?>
+                        <div class="row post-container">
+                          <article class="small-11 columns centered post news-item" aria-labelledby="post-<?php the_ID(); ?>" itemscope="itemscope" itemtype="http://schema.org/BlogPosting" itemprop="blogPost">
+                                <div class="row featured">
+                                    <?php the_post_thumbnail('large'); array('class'  => "floatleft", 'itemprop' => 'image'); ?>
+                                </div>
+                                    <h1 itemprop="headline">
+                                        <a href="<?php the_permalink();?>" title="<?php the_title(); ?>" id="post-<?php the_ID(); ?>"><?php the_title(); ?></a>
+                                    </h1>
+                                    <div class="entry-content" itemprop="text"> 
+                                        <?php the_excerpt(); ?>
+                                    </div>
+                            </article>
+                        </div>
+                <?php endwhile; ?>
+                    </div>
+            <?php endif; ?>
+                <div class="small-12 medium-6 columns panel callout radius10 last">
+                    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                        <h1 class="page-title home">About the <?php echo get_bloginfo ( 'description' ); ?> <?php echo get_bloginfo( 'title' ); ?></h1>
+                        <?php the_content(); ?>
+                    <?php endwhile; endif; ?>
+                </div>
+        </div>
+        <?php if ( $news_query->have_posts() ) : ?>
+            <div class="row">
                  <h3><?php echo $theme_option['flagship_sub_feed_name']; ?></h3>
     	           <?php while ($news_query->have_posts()) : $news_query->the_post(); ?>
     	                <div class="small-12 medium-4 columns post-container">
@@ -87,14 +88,15 @@
     	                    </div>
     	                </div>
     	           <?php endwhile; ?>
-	        </div>
-        <div class="row">
-		    <h5>
-                <a href="<?php echo get_permalink( get_option( 'page_for_posts' ) ); ?>">
-                    View All <?php echo $theme_option['flagship_sub_feed_name']; ?>
-                </a>
-            </h5>
-		</div>
-		<?php endif; ?>
-	</div>
+            </div>
+            <div class="row">
+        	    <h5>
+                    <a href="<?php echo get_permalink( get_option( 'page_for_posts' ) ); ?>">
+                        View All <?php echo $theme_option['flagship_sub_feed_name']; ?>
+                    </a>
+                </h5>
+        	</div>
+    	<?php endif; ?>
+    </div>
+</main>
 <?php get_footer(); ?>
